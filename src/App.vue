0c0d4726b01d7fdbd737479f29e83c0eaefb8546
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-18 11:41:15 +0400

    v-if statments

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <div>
+    <h2 v-if="num===0">The number is zero {{num}}</h2>
+    <h2 v-else-if="num<0">The number is Negative {{num}}</h2>
+    <h2 v-else-if="num>0">The number is possitive {{num}}</h2>
+    <h2 v-else>not a number {{num}}</h2>
 
-      <h2 :id="headingId">heading</h2>
-      <button :disabled="isDisabled">bind</button>
-      <h2 class="underline">Underline text</h2>
-      <h2 class="underline" :class="status">status</h2>
-      <h2 :class="isPromoted && 'promoted'">promoted movie</h2>
-      <h2 :class="isSoldOut ? 'sold-out' : 'new'">is the movie sold out?</h2>
-      <h2 :class="['new', 'promoted']">newly promoted movies</h2>
-      <h2 :class="[isSoldOut ? 'sold-out' : 'new', isPromoted && 'promoted']">arry conditionalmovies</h2>
-      <h2 :class="{
-        promoted: isPromoted,
-        new: !isSoldOut,
-        'sold-out': isSoldOut
+    <template v-if="display">
+      <h2>beka</h2>
+      <h2>vue</h2>
+      <h2>toturials</h2>
+    </template>
 
-      }">object conditional movie</h2>
 
-      <h2 :style="{color: highlightColor, 'font-size': headerSize + 'px', padding: '20px'}">inline stile</h2>
-      <h2 :style="headerStyleObject">this is style object</h2>
-
-      <h2 :style="[baseStyleObject,successStyleObject]">Success style</h2>
-      <h2 :style="[baseStyleObject,dengerStyleObject]">denger style</h2>
   </div>
 </template>
 
@@ -30,36 +21,8 @@ export default {
 
   data(){
     return {
-
-      headingId: 'beka',
-      isDisabled: true,
-      status: 'success',
-      isPromoted: true,
-      isSoldOut: true,
-      highlightColor: 'orange',
-      headerSize: 50,
-      headerStyleObject: {
-        color: 'orange',
-        fontSize: '50px',
-        padding: '20px'
-
-      },
-      baseStyleObject:{
-        fontSize: '50px',
-        padding: '20px'
-      },
-        successStyleObject:{
-        color: 'green',
-        backgroundColor: 'lightgreen',
-        border: '1px solid green'
-
-      },        
-      dengerStyleObject:{
-        color: 'darkred',
-        backgroundColor: 'red',
-        border: '1px solid red'
-
-      },
+      num: 'hi',
+      display: true
     }
   },
 }
@@ -75,17 +38,4 @@ export default {
   margin-top: 60px;
 }
 
-.underline{
-  text-decoration: underline;
-}
-.promoted{
-  font-style: italic;
-}
-
-.new{
-  color: olivedrab;
-}
-.sold-out{
-  color: red;
-}
 </style>
