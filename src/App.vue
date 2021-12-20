68c976bac80cd4c40a3d3cdcd4db50c6979d7441
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-20 14:08:43 +0400

    form inpute types

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,69 @@
 <template>
-  
   <div>
-    <h2>{{name}}</h2>
-    <div>
-      <button @click="changeName($event), incriment(1, $event)">change name</button>
-    </div>
-    <h2>{{count}}</h2>
-    <div>
-      <button @click="incriment(10, $event)">incriment 10</button>
-      <button @click="incriment(5)">incriment 5</button>
+    <pre>{{JSON.stringify(formValues,null,2)}}</pre>
+    <form @submit="submitForm">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="formValues.name">
+      </div>
+      <div>
+        <label for="profile">Profile Summary is here</label>
+        <textarea id="profile" v-model="formValues.profileSummary"></textarea>
+      </div>
 
-      <button @click="decriment(10)">decriment 10 </button>
-      <button @click="decriment(5)">decriment 5</button>
-    </div>
+      <div>
+        <label for="country">country is here</label>
+        <select id="country" v-model="formValues.country">
+          <option value="">select a country</option>
+          <option value="Germany">Germany</option>
+          <option value="India">India</option>
+          <option value="Georgia">Georgia</option>
+          <option value="USA">Singapore</option>
+        </select>
+      </div>
+      <div>
+        <label for="job-location">Job Location</label>
+        <select id="job-location" multiple v-model="formValues.jobLocation" >
+          <option value="">select a country</option>
+          <option value="Germany">Germany</option>
+          <option value="India">India</option>
+          <option value="Georgia">Georgia</option>
+          <option value="USA">Singapore</option>
+        </select>
+      </div>
+      <div>
+          <label>skill set</label>
+          <input type="checkbox" id="html" value="html" v-model="formValues.skillSet"/>
+          <label for="html" >HTML</label>
+          <input type="checkbox" id="css" value="css" v-model="formValues.skillSet"/>
+          <label for="css">css</label>
+          <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet"/>
+          <label for="javascript">javascript</label>
+      </div>
+      <div>
+          <label>experience</label>
+          <input type="radio" id="1-5" value="1-5" v-model="formValues.experience"/>
+          <label for="1-5" >1-5</label>
+          <input type="radio" id="6-10" value="6-10" v-model="formValues.experience"/>
+          <label for="6-10" >6-10</label>
+          <input type="radio" id="10+" value="10+" v-model="formValues.experience"/>
+          <label for="10+" >10+</label>
+
+      </div>
+
+      <div>
+        
+        <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no">
+        <label for="remoteWork"> open to remote work</label>
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+
+    </form>
+
+
   </div>
-  
 </template>
 
 <script>
@@ -23,22 +72,21 @@ export default {
   name: "App",
   data() {
     return{
-      name: 'wishvas',
-      count: 0,
-      num: 5
+      formValues: {
+        name: '',
+        profileSummary: '',
+        country: '',
+        jobLocation: [],
+        remoteWork: 'no',
+        skillSet:[],
+        experience: ''
+      }
     }
   },
   methods:{
-    incriment(num){
-      this.count +=num
-      console.log('Event',event)
-    },    
-    decriment(num){
-      this.count -=num
-    },
-    changeName(){
-      this.name = 'shengo'
-      console.log('Event',event)
+    submitForm(event){
+      event.preventDefault()
+      console.log('Form values name is this one and the rest if inputed data', this.formValues)
     }
   }
 }
@@ -53,4 +101,35 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+  
+}
+
+input label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+
+input[type='text'],
+textarea,
+select{
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.32857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+}
+
+form{
+  padding: 30px;
+}
+
 </style>
